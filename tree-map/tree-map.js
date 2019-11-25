c9248b7659a330d57942bb265e50feeff3e777f9
Abdlrrahman <56532938+Abdlrrahman@users.noreply.github.com>
2019-11-25 09:54:04 +0100

    5

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -48,3 +48,24 @@ Tree.prototype.map = function(func) {
     return addChild(element);
   })
 };
+
+
+
+
+    var root1 = new Tree(1);
+    var branch2 = root1.addChild(2);
+    var branch3 = root1.addChild(3);
+    var leaf4 = branch2.addChild(4);
+    var leaf5 = branch2.addChild(5);
+    var leaf6 = branch3.addChild(6);
+    var leaf7 = branch3.addChild(7);
+    var newTree = root1.map(function (value) {
+      return value * 2;
+    })
+   newTree.value // 2
+   newTree.children[0].value // 4
+   newTree.children[1].value // 6
+   newTree.children[0].children[1].value // 10
+   newTree.children[1].children[1].value // 14
+    root1.value // still 1
+  
